6e9ed5cccea39b612fab49dbfd8ab76537b8cd6a
Tahsina2226 <tahsinatanvin274@gmail.com>
2025-08-05 19:31:29 +0600

    user routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./modules/auth/auth.routes";
+import userRoutes from "./modules/user/user.routes";
+
 const app = express();
 
 app.use(cors());
@@ -9,5 +11,8 @@ app.use(express.json());
 app.get("/", (_req, res) => {
   res.send("Parcel Delivery API is running");
 });
+
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
 export default app;
